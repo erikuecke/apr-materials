b2ee8ac085c843d2f60852d8efbc889e30ba070f
Erik Uecke <erikuecke@gmail.com>
2022-02-03 15:30:59 -0700

    ECS setup

--- 17-ecs-and-collaborative-experience/projects/starter/XOXO/XOXO/ViewController.swift
+++ 17-ecs-and-collaborative-experience/projects/starter/XOXO/XOXO/ViewController.swift
@@ -36,61 +36,143 @@ import ARKit
 import MultipeerConnectivity
 
 class ViewController: UIViewController, ARSessionDelegate {
-  
-  // MARK: - Properties
-  
-  // MARK: - IBOutlets & IBActions
-  
-  @IBOutlet var arView: ARView!
-  @IBOutlet weak var message: UILabel!
-  
-  @IBAction func player1ButtonPressed(_ sender: Any) {
-  }
-  
-  @IBAction func player2ButtonPressed(_ sender: Any) {
-  }
-  
-  @IBAction func clearButtonPressed(_ sender: Any) {
-  }
-  
-  // MARK: - AR View Functions
-  
-  override func viewDidAppear(_ animated: Bool) {
-    super.viewDidAppear(animated)
-  }
+    
+    // MARK: - Properties
+    var playerColor = UIColor.blue
+    var gridModelEntityX: ModelEntity?
+    var gridModelEntityY: ModelEntity?
+    var tileModelEntity: ModelEntity?
+    
+    
+    // MARK: - IBOutlets & IBActions
+    
+    @IBOutlet var arView: ARView!
+    @IBOutlet weak var message: UILabel!
+    
+    @IBAction func player1ButtonPressed(_ sender: Any) {
+        playerColor = UIColor.blue
+    }
+    
+    @IBAction func player2ButtonPressed(_ sender: Any) {
+        playerColor = UIColor.red
+    }
+    
+    @IBAction func clearButtonPressed(_ sender: Any) {
+    }
+    
+    // MARK: - AR View Functions
+    
+    func initARView() {
+        arView.session.delegate = self
+        arView.automaticallyConfigureSession = false
+        let arConfiguration = ARWorldTrackingConfiguration()
+        arConfiguration.planeDetection = [.horizontal]
+        arConfiguration.environmentTexturing = .automatic
+        arView.session.run(arConfiguration)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        initARView()
+        initModelEntities()
+        initGestures()
+    }
 }
 
 // MARK: - Model Entity Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    func initModelEntities() {
+        // 1
+        gridModelEntityX = ModelEntity(mesh: .generateBox(size: SIMD3(x: 0.3, y: 0.01, z: 0.01)), materials: [SimpleMaterial(color: .white, isMetallic: false)])
+        // 2
+        gridModelEntityY = ModelEntity(mesh: .generateBox(size: SIMD3(x: 0.01, y: 0.01, z: 0.3)), materials: [SimpleMaterial(color: .white, isMetallic: false)])
+        // 3
+        tileModelEntity = ModelEntity(mesh: .generateBox(size: SIMD3(x: 0.07, y: 0.01, z: 0.07)), materials: [SimpleMaterial(color: .gray, isMetallic: false)])
+        // 4
+        tileModelEntity!.generateCollisionShapes(recursive: false)
+    }
+    
+    func cloneModelEntity(_ modelEntity: ModelEntity, position: SIMD3<Float>) -> ModelEntity {
+        let newModelEntity = modelEntity.clone(recursive: false)
+        newModelEntity.position = position
+        return newModelEntity
+    }
+    
+    func addGameBoardAnchor(transform: simd_float4x4) {
+        // 1
+        let arAnchor = ARAnchor(name: "XOXO Grid", transform: transform)
+        let anchorEntity = AnchorEntity(anchor: arAnchor)
+        // 2
+        anchorEntity.addChild(cloneModelEntity(gridModelEntityY!, position: SIMD3(x: 0.05, y: 0, z: 0)))
+        anchorEntity.addChild(cloneModelEntity(gridModelEntityY!, position: SIMD3(x: -0.05, y: 0, z: 0)))
+        anchorEntity.addChild(cloneModelEntity(gridModelEntityX!, position: SIMD3(x: 0, y: 0, z: 0.5)))
+        anchorEntity.addChild(cloneModelEntity(gridModelEntityX!, position: SIMD3(x: 0.05, y: 0, z: -0.5)))
+        
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: -0.1, y: 0, z: -0.1)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0, y: 0, z: -0.1)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0.1, y: 0, z: -0.1)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: -0.1, y: 0, z: 0)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0, y: 0, z: 0)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0.1, y: 0, z: 0)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: -0.1, y: 0, z: 0.1)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0, y: 0, z: 0.1)))
+        anchorEntity.addChild(cloneModelEntity(tileModelEntity!,
+          position: SIMD3(x: 0.1, y: 0, z: 0.1)))
+
+        // 1
+        anchorEntity.anchoring = AnchoringComponent(arAnchor)
+        // 2
+        arView.scene.addAnchor(anchorEntity)
+        arView.session.add(anchor: arAnchor)
+        
+    }
 }
 
 // MARK: - Gesture Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    func initGestures() {
+        // 1
+        let tap = UITapGestureRecognizer(target: self, action: #selector(handleTap))
+        self.arView.addGestureRecognizer(tap)
+    }
+    
+    @objc func handleTap(recognizer: UITapGestureRecognizer?) {
+        
+    }
+    
 }
 
 
 // MARK: - Multipeer Session Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    // Add code here...
+    
 }
 
 // MARK: - Helper Functions
 
 extension ViewController {
-  
-  // Add code here...
-  
+    
+    func sendMessage(_ message: String) {
+        DispatchQueue.main.async {
+            self.message.text = message
+        }
+    }
+    
 }
 
 
